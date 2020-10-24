ec107722011c6f8fcdc7412737f1c9b1be7e494a
Pawel Tomkow <paweltomkow@gmail.com>
2020-10-24 13:32:02 +0200

    docker init

---
+++ run_docker.bat
@@ -0,0 +1,5 @@
+@echo off
+docker-compose up -d --build
+echo "Mongo port 47017, L:root P:root"
+echo "postgres port 15432, L:root P:root"
+echo "pgadmin port 15431, L:root P:root"
---
+++ run_docker.sh
@@ -0,0 +1,6 @@
+#!\bin\bash\
+
+docker-compose up -d --build
+echo "Mongo port 47017, L:root P:root"
+echo "postgres port 15432, L:root P:root"
+echo "pgadmin port 15431, L:root P:root"
