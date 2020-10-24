8cf49a47199ae3c1ed7e42a81e517a4c18597d48
Pawel Tomkow <paweltomkow@gmail.com>
2020-10-24 14:28:11 +0200

    fix pgadmin

--- run_docker.bat
+++ run_docker.bat
@@ -1,7 +1,7 @@
 @echo off
 docker-compose up -d --build
+docker run -p 15431:80 -e "PGADMIN_DEFAULT_EMAIL=root" -e "PGADMIN_DEFAULT_PASSWORD=root" -d dpage/pgadmin4 --name "rpk_pgadmin"
 echo "Mongo port 47017, L:root P:root"
 echo "postgres port 15432, L:root P:root"
 echo "pgadmin port 15431, L:root P:root"
-
 pause
